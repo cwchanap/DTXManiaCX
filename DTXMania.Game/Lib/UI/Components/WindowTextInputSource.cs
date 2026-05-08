bf3f19e619a039ab1661b9668461ccd9c9abb4cd
cwchanap <cwchanap@connect.ust.hk>
2026-05-08 10:47:25 -0700

    feat: add ITextInputSource abstraction and GameWindow adapter

---
+++ DTXMania.Game/Lib/UI/Components/WindowTextInputSource.cs
@@ -0,0 +1,25 @@
+#nullable enable
+
+using System;
+using Microsoft.Xna.Framework;
+
+namespace DTXMania.Game.Lib.UI.Components
+{
+    public sealed class WindowTextInputSource : ITextInputSource
+    {
+        private readonly GameWindow _window;
+
+        public WindowTextInputSource(GameWindow window)
+        {
+            _window = window ?? throw new ArgumentNullException(nameof(window));
+            _window.TextInput += OnTextInput;
+        }
+
+        public event EventHandler<TextInputEventArgs>? TextInput;
+
+        private void OnTextInput(object? sender, TextInputEventArgs e)
+        {
+            TextInput?.Invoke(sender, e);
+        }
+    }
+}
