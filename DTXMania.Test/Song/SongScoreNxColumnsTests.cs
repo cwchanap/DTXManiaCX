af0be629cc41ca4a3d6705fb7137d974632e0105
cwchanap <cwchanap@connect.ust.hk>
2026-06-08 23:32:20 -0700

    feat: add NxImported snapshot columns to SongScore

---
+++ DTXMania.Test/Song/SongScoreNxColumnsTests.cs
@@ -0,0 +1,62 @@
+using System.Threading.Tasks;
+using DTXMania.Game.Lib.Song.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using SongEntity = DTXMania.Game.Lib.Song.Entities.Song;
+using Xunit;
+
+namespace DTXMania.Test.Song
+{
+    [Trait("Category", "Unit")]
+    public class SongScoreNxColumnsTests : System.IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        private readonly DbContextOptions<SongDbContext> _options;
+
+        public SongScoreNxColumnsTests()
+        {
+            _connection = new SqliteConnection("Data Source=:memory:;Foreign Keys=True");
+            _connection.Open();
+            _options = new DbContextOptionsBuilder<SongDbContext>().UseSqlite(_connection).Options;
+            using var setup = new SongDbContext(_options);
+            setup.Database.EnsureCreated();
+        }
+
+        public void Dispose() => _connection.Dispose();
+
+        [Fact]
+        public async Task SongScore_PersistsNxImportedCounts()
+        {
+            int chartId;
+            using (var ctx = new SongDbContext(_options))
+            {
+                var chart = new SongChart { Song = new SongEntity { Title = "S" }, FilePath = "a.dtx" };
+                ctx.SongCharts.Add(chart);
+                await ctx.SaveChangesAsync();
+                chartId = chart.Id;
+                ctx.SongScores.Add(new SongScore
+                {
+                    ChartId = chartId, Instrument = EInstrumentPart.DRUMS,
+                    NxImportedPlayCount = 79, NxImportedClearCount = 72
+                });
+                await ctx.SaveChangesAsync();
+            }
+
+            using (var ctx = new SongDbContext(_options))
+            {
+                var saved = await ctx.SongScores.AsNoTracking().FirstAsync(s => s.ChartId == chartId);
+                Assert.Equal(79, saved.NxImportedPlayCount);
+                Assert.Equal(72, saved.NxImportedClearCount);
+            }
+        }
+
+        [Fact]
+        public void Clone_CopiesNxImportedCounts()
+        {
+            var score = new SongScore { NxImportedPlayCount = 5, NxImportedClearCount = 3 };
+            var clone = score.Clone();
+            Assert.Equal(5, clone.NxImportedPlayCount);
+            Assert.Equal(3, clone.NxImportedClearCount);
+        }
+    }
+}
