8f2fdb6dbaef330f6cae51e7df32658aaf081fc7
Chan Wai Chan <cwchanap@connect.ust.hk>
2026-09-04 17:49:08 -0700

    test: remove misleading mac folder picker owner seam

--- DTXMania.Game/Platform/FolderPickerServiceFactory.Mac.cs
+++ DTXMania.Game/Platform/FolderPickerServiceFactory.Mac.cs
@@ -1,6 +1,5 @@
 #nullable enable
 
-using System;
 using DTXMania.Game.Lib.Stage.Config;
 
 namespace DTXMania.Game.Platform
@@ -8,7 +7,5 @@ namespace DTXMania.Game.Platform
     internal static class FolderPickerServiceFactory
     {
         internal static IFolderPickerService Create() => new MacFolderPickerService();
-
-        internal static IFolderPickerService Create(IntPtr _) => new MacFolderPickerService();
     }
 }
