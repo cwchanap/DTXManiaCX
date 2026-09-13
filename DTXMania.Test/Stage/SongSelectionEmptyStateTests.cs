49f46aa388bd42aab6f6726b983f10048ba21743
Chan Wai Chan <cwchanap@connect.ust.hk>
2026-09-12 17:24:29 -0700

    test: enforce empty-state width budget

--- DTXMania.Test/Stage/SongSelectionEmptyStateTests.cs
+++ DTXMania.Test/Stage/SongSelectionEmptyStateTests.cs
@@ -1,5 +1,9 @@
+using DTXMania.Game.Lib.Resources;
 using DTXMania.Game.Lib.Stage;
 using DTXMania.Game.Lib.UI.Layout;
+using DTXMania.Game.Lib.Utilities;
+using Microsoft.Xna.Framework;
+using Moq;
 using Xunit;
 
 namespace DTXMania.Test.Stage
@@ -40,6 +44,27 @@ namespace DTXMania.Test.Stage
 
             Assert.NotEmpty(message);
             Assert.All(message, character => Assert.InRange((int)character, 0x20, 0x7e));
+        }
+
+        [Theory]
+        [InlineData((int)SongSelectionStage.SongLibraryEmptyState.NoActiveRoots)]
+        [InlineData((int)SongSelectionStage.SongLibraryEmptyState.NoSupportedCharts)]
+        public void ResolveLibraryEmptyMessage_RenderedCopy_ShouldFitSongBarWidth(int stateValue)
+        {
+            var font = new Mock<IFont>();
+            font.Setup(value => value.MeasureString(It.IsAny<string>()))
+                .Returns<string>(text => new Vector2(text.Length * 8f, 14f));
+            var state = (SongSelectionStage.SongLibraryEmptyState)stateValue;
+
+            var rendered = TextHelper.TruncateToWidth(
+                SongSelectionStage.ResolveLibraryEmptyMessage(state),
+                SongSelectionUILayout.SongBars.EmptyMessageMaxWidth,
+                font.Object);
+
+            Assert.NotEmpty(rendered);
+            Assert.True(
+                font.Object.MeasureString(rendered).X <= SongSelectionUILayout.SongBars.EmptyMessageMaxWidth,
+                $"rendered recovery copy \"{rendered}\" must fit the song-bar width budget");
         }
 
         [Fact]
